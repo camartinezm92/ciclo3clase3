4389ee7b5e66db34a874ad238b2110cd64af3715
Camilomartinez <camartinezm92@gmail.com>
2022-08-17 21:41:55 -0500

    version inicial de esta vaina DIOS 2.0

--- src/mi/protyecto/MiProtyecto.java
+++ src/mi/protyecto/MiProtyecto.java
@@ -16,8 +16,10 @@ public class MiProtyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Aprendiendo a compartir informacion con la clase");
-        
-        
+
+        System.out.println("Aprendiendo a compartir informacion con la clase");
+        System.out.println("Aprendiendo a compartir informacion con la clase");
+
     }
     
 }
