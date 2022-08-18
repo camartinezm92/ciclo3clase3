4bdd97ba5bd0d1fd906b6d5d0d845f0eb567d57d
Camilomartinez <camartinezm92@gmail.com>
2022-08-17 21:24:46 -0500

    version inicial de esta vaina DIOSgit status -sgit status -s!

---
+++ src/mi/protyecto/MiProtyecto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mi.protyecto;
+
+/**
+ *
+ * @author CM
+ */
+public class MiProtyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Aprendiendo a compartir informacion con la clase");
+        
+        
+    }
+    
+}
