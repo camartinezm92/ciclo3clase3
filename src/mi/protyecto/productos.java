9fa3d51e4858dcc5098dd09949664ddfe4bef05c
Camartinezm92 <Camartinezm92@gmail.com>
2022-08-18 20:15:32 -0500

    v3-Creacion clase productos y estableciemiento de constructor

---
+++ src/mi/protyecto/productos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mi.protyecto;
+
+/**
+ *
+ * @author CM
+ */
+public class productos {
+    
+    private String nombre, categoria;
+    private int precio;
+
+    public productos(String nombre, String categoria, int precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }  
+}
